96414e9274dbf09178fa40c0e7989e3a362db2d0
rafalkaplon-code <rafal.kaplon@o2.pl>
2022-07-05 21:01:38 +0200

    Rebuilid home page

--- smartmachine/smartmachine/views.py
+++ smartmachine/smartmachine/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from app_machine.models import Station
 
 
 def dashboard_view(request):
     """
     Main view
     """
-    context = {}
+    stations = Station.objects.all()
+    stats = []
+
+    for st in stations:
+        stats.append({st, st.status.last()})
+
+
+    context = {
+        'stations': stations,
+        'stats': stats,
+    }
     return render(request, 'home/home.html', context)
